327653fb6d302f1d45e9e3c3268b0351eb60d087
"frenchkebab" <"tubowangin@gmail.com">
2022-07-14 14:05:36 +0900

    [#4] Changes ERC20.sol structure (openzeppelin)

--- week2-erc20/assignment4/ERC20.sol
+++ week2-erc20/assignment4/ERC20.sol
@@ -4,11 +4,11 @@ pragma solidity ^0.8.7;
 interface IERC20 {
     function totalSupply() external view returns (uint);
 
-    function balanceOf(address account) external view returns(uint);
+    function balanceOf(address account) external view returns (uint256);
 
     function transfer(address recipient, uint amount) external returns (bool);
 
-    function allowance(address owner, address spender) external view returns (uint);
+    function allowance(address owner, address spender) external view returns (uint256);
 
     function approve(address spender, uint amount) external returns (bool);
 
@@ -20,48 +20,114 @@ interface IERC20 {
 
 // this contract implements all functions above
 contract ERC20 is IERC20 {
-    uint public override totalSupply;
-    mapping(address => uint) public override balanceOf;
-    mapping(address => mapping(address => uint)) public override allowance;
-    string public name = "Test";
-    string public symbol = "TEST";
-    uint8 public decimals = 18;
+    uint public _totalSupply;
+    mapping(address => uint) private _balances;
+    mapping(address => mapping(address => uint)) private _allowances;
+    string private _name = "Test";
+    string private _symbol = "TEST";
+    uint8 private _decimals = 18;
 
+
+    /**
+      * getter functions here
+      */
+    function name() public view returns (string memory) {
+        return _name;
+    }
+
+    function symbol() public view returns (string memory) {
+        return _symbol;
+    }
+
+    function decimals() public view returns (uint256) {
+        return _decimals;
+    }
+
+    function totalSupply() public view override returns (uint256) {
+        return _totalSupply;
+    }
+
+    function balanceOf(address account) public view override returns (uint256) {
+        return _balances[account];
+    }
+
+    function allowance(address owner, address spender) public view override returns (uint256) {
+        return _allowances[owner][spender];
+    }
+
+
+    /**
+      * setter functions here
+      */
     // question: virtual
-    function transfer(address recipient, uint amount) public virtual override returns (bool) {
-        balanceOf[msg.sender] -= amount;
-        balanceOf[recipient] += amount;
-        emit Transfer(msg.sender, recipient, amount);
+    function transfer(address to, uint amount) public virtual override returns (bool) {
+        _transfer(msg.sender, to, amount);
         return true;
     }
 
     function approve(address spender, uint amount) external override returns (bool) {
-        allowance[msg.sender][spender] = amount;
-        emit Approval(msg.sender, spender, amount);
+        _approve(msg.sender, spender, amount);
         return true;
     }
 
     function transferFrom(
-        address sender,
-        address recipient,
+        address from,
+        address to,
         uint amount
     ) external override returns (bool) {
-        allowance[sender][msg.sender] -= amount;
-        balanceOf[sender] -= amount;
-        balanceOf[recipient] += amount;
-        emit Transfer(sender, recipient, amount);
+        address spender = msg.sender;
+        _spendAllowance(from, spender, amount);
+        _transfer(from, to, amount);
         return true;
     }
 
     function mint(uint amount) external {
-        balanceOf[msg.sender] += amount;
-        totalSupply += amount;
+        _balances[msg.sender] += amount;
+        _totalSupply += amount;
         emit Transfer(address(0), msg.sender, amount);
     }
 
     function burn(uint amount) external {
-        balanceOf[msg.sender] -= amount;
-        totalSupply -= amount;
+        _balances[msg.sender] -= amount;
+        _totalSupply -= amount;
         emit Transfer(msg.sender, address(0), amount);
+    }
+
+    // Newly Added
+
+    function _transfer(
+        address from,
+        address to,
+        uint256 amount
+    ) internal {
+        require(from != address(0), "Transfer from the zero address");
+        require(to != address(0), "Transfer to the zero address");
+
+        uint256 fromBalance = _balances[from];
+        require(fromBalance >= amount, "Transfer amount exceeds balance");
+
+        emit Transfer(from, to, amount);
+    }
+
+    function _approve(
+        address owner,
+        address spender,
+        uint256 amount
+    ) internal {
+        require(owner != address(0), "Approve from the zero address");
+        require(spender != address(0), "Approve to the zero address");
+
+        _allowances[owner][spender] = amount;
+        emit Approval(owner, spender, amount);
+    }
+
+    function _spendAllowance(
+        address owner,
+        address spender,
+        uint256 amount
+    ) internal {
+        uint256 currentAllowance = _allowances[owner][spender];
+        require(currentAllowance >= amount, "Insufficient Allowance");
+        _approve(owner, spender, currentAllowance - amount);
     }
 }
